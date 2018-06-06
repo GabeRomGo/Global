60ed64c6a579362595caa220e28a7f28c0de2b03
Gabe Romero <30396807+GabeRomGo@users.noreply.github.com>
2018-06-06 00:38:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] records = GetInts();
+            int[] sortedRecords;
+            if (records.Length >= 100) 
+            {
+                sortedRecords = SortRecords(records, "Merge");                
+            }
+            else
+            {
+                sortedRecords = SortRecords(records, "Insertion");              
+            }
+
+            Console.WriteLine("There are {0} sorted records", sortedRecords.Length);
+            for (int i = 0; i < sortedRecords.Length; i++)
+            {
+                var shiftedValue = sortedRecords[i] * 10;
+                Console.WriteLine("The shifted value at position {0} is {1}", i, shiftedValue);
+            }
+        }
+
+        private static int[] GetInts()
+        {
+            // implementation is not relevant
+            throw new NotImplementedException();
+        }
+
+        private static int[] SortRecords(int[] records, string sortAlgorithm)
+        {
+            // implementation is not relevant
+            throw new NotImplementedException();
+        }
+    }
+}
